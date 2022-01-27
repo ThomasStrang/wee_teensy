44eba54172759330535dae0f18320a21033762dc
Thomas Strang <strangthomas96@gmail.com>
2022-01-27 17:42:41 +0000

    added basic voltage reading display

--- display_numbers/display_numbers.ino
+++ display_numbers/display_numbers.ino
@@ -1,14 +1,29 @@
 
+#define INPUT_PIN 22
+#define OUTPUT_PIN 21
+
 void setup() {
   if(setup_display()) {
     Serial.println("ERROR, setup_display() != 0");
   }
+  pinMode(INPUT_PIN,INPUT);
+  pinMode(OUTPUT_PIN,OUTPUT);
+  digitalWrite(OUTPUT_PIN,HIGH);
+}
+
+void print_number(int n) {
+  int ones = n%10;
+  int tens = (n/10)%10;
+  int hundreds = (n/100)%10;
+  int thousands = (n/1000)%10;
+  light_digits(thousands,hundreds,tens,ones);
 }
 
 void loop() {
-  //this function call takes 200us
-  //the brightness of the leds scales with with how often this function is called.
-  //It is best to run this in it's own thread.
-  light_digits(1,2,3,4);
-  delay(1);
+  int input_value = analogRead(INPUT_PIN);
+  int start = millis();
+  while(millis()-start < 500) {
+    print_number(input_value);
+    delay(1);
+  }
 }
