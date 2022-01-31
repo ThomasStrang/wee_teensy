fb159bb5d86aa0aa03174a75e705e07cf458c83f
Thomas Strang <strangthomas96@gmail.com>
2022-01-31 13:59:27 +0000

    oscilloscope features for reading pwm

--- junk/pwm_read_write/pwm_read_write.ino
+++ junk/pwm_read_write/pwm_read_write.ino
@@ -1,38 +1,66 @@
-#define PIN_OUT 14
+#define PIN_OUT 11
 #define PIN_IN 15
+#define PIN_IN_5V 16
+#define PIN_IN_12V 17
+
+IntervalTimer update_pwm_width_timer;
+
+int delay_micros = 1;
+bool analog = false;
+int pixels_per_reading = 1;
+int screen_delay_ms = 250;
+int pin_in = PIN_IN;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(PIN_IN, INPUT);
-  calibrate_normalise_val();
+  analogWrite(PIN_OUT,10);
+  update_pwm_width_timer.begin(update_pwm_width, 50000);
 }
 
-float normalise_val = 0.333;
+int pwm_width=16;
 
-void calibrate_normalise_val() {
-  normalise_val=1.0/3.0;
-  analogWrite(PIN_OUT,255);
-  float raw = read_val();
-  normalise_val = (255.0/raw)/3;
+void update_pwm_width() {
+  pwm_width+=1;
+  if(pwm_width >= 256) pwm_width = 0;
+  analogWrite(PIN_OUT,pwm_width);
 }
 
-int read_val() {
-  int first = pulseIn(PIN_IN, HIGH);
-  int second = pulseIn(PIN_IN, HIGH);
-  int third = pulseIn(PIN_IN, HIGH);
-  
-  return (int)((first+second+third)*normalise_val);
+float digital_to_voltage_calibration = 1.0/1024*3.3;
+
+float convert_to_volts(int val) {
+  return digital_to_voltage_calibration*val;
+}
+
+void sync_with_signal() {
+  int now = micros();
+  while(digitalRead(pin_in)) 
+    if(micros() - now > 1000) 
+      break;
+  while(!digitalRead(pin_in)) 
+    if(micros() - now > 1000) 
+      break;
+}
+
+
+void print_vals() {
+  for(int i = 0; i < 500; i++) {
+    if(!(i%pixels_per_reading)) {
+      int val = get_val();
+      for(int j = 0; j < pixels_per_reading && j+i<500; j++) {
+        Serial.println(val);
+      }
+      delayMicroseconds(delay_micros);
+    }
+  }
+}
+
+int get_val() {
+   return analog ? analogRead(pin_in) : digitalRead(pin_in);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for(int i = 0; i < 256; i++) {
-    analogWrite(PIN_OUT,i);
-    int val = read_val();
-    Serial.print("\nvalue sent: ");
-    Serial.print(i);
-    Serial.print(" value read: ");
-    Serial.print(val);
-    delay(100);
-  }
+  sync_with_signal();
+  print_vals();
+  delay(screen_delay_ms);
+  handle_input();
 }
---
+++ junk/pwm_read_write/serial_input_handler.ino
@@ -0,0 +1,83 @@
+//first char is the control char, the rest is an integer.
+#define UPDATE_FREQUENCY 'F'
+#define UPDATE_READINGS 'R'
+#define UPDATE_FPS 'S'
+
+#define ANALOG 'A'
+#define DIGITAL 'D'
+#define THREE_VOLTS '3'
+#define FIVE_VOLTS '5'
+#define TWELVE_VOLTS '2'
+
+int read_int_from_serial() {
+  int val=0;
+  while(Serial.available()) {
+    byte b = Serial.read();
+    if(b == 10) break;
+    val = val*10;
+    val += b - '0';
+  }
+  return val;
+}
+
+void update_readings() {
+  int val = read_int_from_serial();
+  if(val>0 && val <=500) 
+    pixels_per_reading=500/val;
+}
+
+void update_frequency() {
+  delay_micros=read_int_from_serial();
+}
+void update_fps() {
+  screen_delay_ms=1000/read_int_from_serial();
+}
+
+void set_to_analog() {
+  pinMode(PIN_IN, INPUT_DISABLE);
+  analog=true;
+}
+
+void set_to_digital() {
+  pinMode(PIN_IN, INPUT);
+  analog=false;
+}
+
+void update_voltage_mode() {
+  int val = read_int_from_serial();
+  if(val>0 && val <=500) 
+    pixels_per_reading=500/val;
+  
+}
+
+void handle_input() {
+  if(Serial.available()) {
+    switch(Serial.read()) {
+      case UPDATE_FREQUENCY :
+        update_frequency();
+        break;
+      case UPDATE_READINGS :
+        update_readings();
+        break;
+      case UPDATE_FPS :
+        update_fps();
+        break;
+      case ANALOG :
+        set_to_analog();
+        break;
+      case DIGITAL :
+        set_to_digital();
+        break;
+      case THREE_VOLTS :
+        pin_in=PIN_IN;
+        break;
+      case FIVE_VOLTS :
+        pin_in=PIN_IN_5V;
+        break;
+      case TWELVE_VOLTS :
+        pin_in=PIN_IN_12V;
+        break;        
+    }
+    while(Serial.available()) Serial.read();
+  }
+}
